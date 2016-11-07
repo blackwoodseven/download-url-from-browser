1ac13ceafc6b54c2091da4255545642555cac870
Hibai Unzueta <hibai.unzueta@gmail.com>
2016-11-07 13:06:23 +0100

    Not using ES2015 now

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = url => {
-  const link = document.createElement('a')
+module.exports = function(url) {
+  var link = document.createElement('a')
   link.download = name
   link.href = url
 
-  const clickEvent = new MouseEvent('click', {
+  var clickEvent = new MouseEvent('click', {
     'view': window,
     'bubbles': true,
     'cancelable': false
